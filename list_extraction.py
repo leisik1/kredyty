ec4ab781ab0fc989994f283a2be2f4c8c4a0982c
w-smialek <79923438+w-smialek@users.noreply.github.com>
2023-03-12 20:04:46 +0100

    moje

--- list_extraction.py
+++ list_extraction.py
@@ -39,17 +39,43 @@ id_and_def = {}
 #         else:
 #             id_and_def[row[0]].append(row[-1])
 
-with open("sorted_data.csv") as file:
-    output = csv.reader(file, delimiter=";", quotechar='"')
-    for row in output:
-        if row[0] not in id_and_def.keys():
-            id_and_def[row[0]] = []
-            for val in range(42):
-                id_and_def[row[0]].append(row[val])
-        else:
-            for val in range(42):
-                id_and_def[row[0]].append(row[val])
+# with open("sorted_data.csv") as file:
+#     output = csv.reader(file, delimiter=";", quotechar='"')
+#     for row in output:
+#         if row[0] not in id_and_def.keys():
+#             id_and_def[row[0]] = []
+#             for val in range(42):
+#                 id_and_def[row[0]].append(row[val])
+#         else:
+#             for val in range(42):
+#                 id_and_def[row[0]].append(row[val])
+
+# file.close()
+
+# print(id_and_def["2897"])
+
+###WS
+
+with open(".\sorted_data.csv") as fp:
+    reader = csv.reader(fp, delimiter=";", quotechar='"')
+    jebutna_lista = []
+    for i in range(100):#148900
+        jebutna_lista.append(next(reader))
 
 
-print(id_and_def["2897"])
+companies = {}
+id_now = "0"
 
+for line in jebutna_lista:
+    if id_now != line[0]:
+        id_now = line[0]
+        companies[id_now] = [line[1:]]
+    else:
+        companies[id_now].append(line[1:])
+
+print(companies)
+
+companies_def = {k:v for (k,v) in companies.items() if True in [bool(int(val[-1])) for val in companies[k]]}
+companies_nondef = {k:v for (k,v) in companies.items() if not (True in [bool(int(val[-1])) for val in companies[k]])}
+
+print(companies_def)
--- wojtka.py
+++ wojtka.py
@@ -135,3 +135,21 @@ plt.close(fig1)
 
 
 # print(companies)
+
+
+### Kullback–Leibler divergence
+
+
+
+example1 = [0,0,1,2,3,4,4,3,2,1,0,0,0,0,0,0,0]
+example2 = [0,0,0,0,0,0,0,0,1,1,2,2,3,3,3,3,2]
+
+def KLD(data0,data1):
+    data1 = data1 / scipy.linalg.norm(data1,order=1)
+    data0 = data0 / scipy.linalg.norm(data0,order=1)
+    kld_data = []
+    for i in range(len(example1)):
+        kld_data.append(data1[i]*np.log(data1[i]/data0[i]))
+    return sum(kld_data)
+
+print(KLD(example1,example2))
