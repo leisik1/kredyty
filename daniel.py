75d671f521cc80d854b3f5bedc8226790dcc150c
leisik1 <daniel.kisiel15@gmail.com>
2023-03-12 19:49:52 +0100

    gowno dziala

--- daniel.py
+++ daniel.py
@@ -4,7 +4,7 @@ data_frame = pd.read_table("sorted_data.csv", delimiter=";")
 
 #Sorted data CSV has two depth sort - first the ID and second the Date from oldest to newest
 
-print(data_frame.loc[1:3, ['ID', 'default']])
+# print(data_frame.loc[1:3, ['ID', 'default']])
  
 
 groups = data_frame['ID'].unique()
@@ -14,4 +14,12 @@ for group in groups:
     name = "ID" + str(group)
     subsets[name] = data_frame[data_frame['ID'] == group]
 
-print(subsets["ID9353"])
+# for val in subsets["ID9353"]["default"]
+
+print(subsets["ID9353"]["default"].values)
+
+for val in subsets["ID9353"]["default"].values:
+    print(val)
+
+
+
---
+++ list_extraction.py
@@ -0,0 +1,55 @@
+# import numpy as np
+# import scipy
+# import matplotlib.pyplot as plt
+# import csv
+# import statistics
+
+# with open("sorted_data.csv") as fp:
+#     reader = csv.reader(fp, delimiter=";", quotechar='"')
+#     data_read = []
+#     for i in range(100000):#148900
+#         data_read.append(next(reader))
+
+# companies = {}
+# id_now = "0"
+
+# for line in data_read:
+#     if id_now != line[0]:
+#         id_now = line[0]
+#         companies[id_now] = [line[1:]]
+#     else:
+#         companies[id_now].append(line[1:])
+
+# companies_def = {k:v for (k,v) in companies.items() if True in [bool(int(val[-1])) for val in companies[k]]}
+# companies_nondef = {k:v for (k,v) in companies.items() if not (True in [bool(int(val[-1])) for val in companies[k]])}
+
+# print(companies_def)
+
+import csv 
+
+
+id_and_def = {}
+
+# with open("sorted_data.csv") as file:
+#     output = csv.reader(file, delimiter=";", quotechar='"')
+#     for row in output:
+#         if row[0] not in id_and_def.keys():
+#             id_and_def[row[0]] = []
+#             id_and_def[row[0]].append(row[-1])
+#         else:
+#             id_and_def[row[0]].append(row[-1])
+
+with open("sorted_data.csv") as file:
+    output = csv.reader(file, delimiter=";", quotechar='"')
+    for row in output:
+        if row[0] not in id_and_def.keys():
+            id_and_def[row[0]] = []
+            for val in range(42):
+                id_and_def[row[0]].append(row[val])
+        else:
+            for val in range(42):
+                id_and_def[row[0]].append(row[val])
+
+
+print(id_and_def["2897"])
+
